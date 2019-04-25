5a03630b92f8c723195dc0be480822cce545a602
Daniel Loomer <fuzetsu490@gmail.com>
2019-04-25 10:53:14 -0400

    test app improvements: - track runtime in test page and display - support switching debug mode on/off using querystring - add some nested media queries - switch over to the pseudo child sel shorthand - add keyframe animation - display rules even in prod mode

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,27 @@
 import { html, render, Component } from 'https://unpkg.com/htm/preact/standalone.mjs'
-import z from '../src/index.js'
+import _z from '../src/index.js'
 // import z from '../dist/zaftig.min.js'
 
-z.setDebug(true)
+// const p = (...args) => (console.log(...args), args[0])
+
+let runTime = 0
+const z = Object.assign((...args) => {
+  const start = performance.now()
+  try {
+    return _z(...args)
+    // eslint-disable-next-line no-empty
+  } catch (e) {
+  } finally {
+    runTime += performance.now() - start
+  }
+}, _z)
+
+const params = new URLSearchParams(location.search)
+const isDebug = params.get('debug') !== 'false'
+z.setDebug(isDebug)
 
 z.add('html,body', 'm 0;ff sans-serif;bc #445566;c white;fs 14px')
-  .add('a', 'c lightblue; &:visited { c blue }')
+  .add('a', 'c lightblue; &:visited { c limegreen }')
   .add('pre', 'm 0')
 
 z.helper({
@@ -15,6 +31,8 @@ z.helper({
   shadow: num => `box-shadow 0 0 ${num}px 0 rgba(0,0,0,0.5)`
 })
 
+window.z = z
+
 const btn = z`
   d inline-block
   br 4px
@@ -23,7 +41,16 @@ const btn = z`
   fs 16px
   us none;cursor pointer
   transition transform 100ms
-  &:active { transform scale(0.9) }
+  :active { transform scale(0.9) }
+  @media (max-width: 800px) {
+    pad 5
+    color orange
+    @media screen {
+      @media print {
+        color green
+      }
+    }
+  }
 `
 
 const tbox = z`
@@ -33,10 +60,14 @@ const tbox = z`
   br 4px
   fs 16px
   transition transform 400ms
-  &:focus { transform scale(1.08) }
+  :focus { transform scale(1.08) }
   &:focus + & {
     transform rotate(180deg)
-    &::placeholder { c orange }
+    ::placeholder { c orange }
+    @media (max-width: 800px) {
+      transform rotate(200deg)
+      ::placeholder { c purple }
+    }
   }
 `
 
@@ -57,7 +88,18 @@ class App extends Component {
           `bc ${color}`
         )}
       >
-        <div class=${z`fs 2em;fw bold;mar 3`}>Zaftig</div>
+        <div
+          class=${z`
+            fs 2em;fw bold;mar 3
+            animation spin 3s linear infinite
+            @keyframes spin {
+              50% { transform rotate(180deg) scale(2) }
+              100% { transform rotate(360deg) scale(1) }
+            }
+          `}
+        >
+          Zaftig
+        </div>
         <div>
           <span class=${btn} onclick=${() => this.setState({ count: count + 1 })}>Increase</span>
           <span class=${z`w 60px;d inline-block;ta center;fs 2em`}>${count}</span>
@@ -73,24 +115,43 @@ class App extends Component {
           class=${tbox}
           onchange=${({ target: t }) => this.setState({ exp: t.value.trim() })}
         />
-        <div class=${z`pad 2;fs 1.5em;ff monospace`}>
-          ${style.valueOf()} ${' = '}
-          ${style.style ||
-            html`
-              <em>type something</em>
-            `}
+        <div class=${z`pad 2;fs 1.5em;ff monospace;ta center`}>
+          <p>
+            ${style.valueOf()} ${' = '}
+            ${style.style ||
+              html`
+                <em>type something</em>
+              `}
+          </p>
+          <p>
+            zaftig runtime ${' '}
+            ${html`
+              <a href=${isDebug ? '?debug=false' : '?debug=true'}>${isDebug ? 'DEBUG' : 'PROD'}</a>
+            `}:
+            ${' ' + runTime.toFixed(3)}ms
+          </p>
         </div>
         <pre
           class=${z`
             ws pre-wrap
             bc white;c #445566
             pad 8
+            mar 4
             br 4px
-            columns 2
             shadow 10
+            columns ${isDebug ? 2 : 1}
           `}
         >
-        ${z.getSheet().textContent.trim()}</pre
+        ${isDebug
+            ? z.getSheet().textContent.trim()
+            : Array.from(
+                z.getSheet().sheet.cssRules,
+                rule =>
+                  html`
+                    <p>${rule.cssText}</p>
+                  `
+              )}
+        </pre
         >
       </main>
     `
