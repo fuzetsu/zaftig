f1e0d9aa4e9620fd0229108259f9187f48dd4af7
Daniel Loomer <fuzetsu490@gmail.com>
2019-04-25 15:10:08 -0400

    include gzip in build stats

--- build.js
+++ build.js
@@ -3,6 +3,7 @@
 const fss = require('fs')
 const fs = fss.promises
 const path = require('path')
+const zlib = require('zlib')
 const Terser = require('terser')
 
 const input = fss.readFileSync('src/index.js', 'utf8')
@@ -22,24 +23,30 @@ const output = Terser.minify(
   }
 )
 
+const reportDiff = (path, newCode) => {
+  if (!fss.existsSync(path)) return
+  const beforeSize = fss.statSync(path).size
+  const beforeGzip = zlib.gzipSync(fss.readFileSync(path)).byteLength
+  const afterSize = newCode.length
+  const afterGzip = zlib.gzipSync(Buffer.from(newCode, 'utf-8')).byteLength
+  console.log(
+    [
+      '== zaftig.min.js ==',
+      'Before: ' + beforeSize + ' (gzip ' + beforeGzip + ')',
+      ' After: ' + afterSize + ' (gzip ' + afterGzip + ')',
+      '  Diff: ' + (beforeSize - afterSize) + ' (gzip ' + (beforeGzip - afterGzip) + ')',
+      '==================='
+    ].join('\n')
+  )
+}
+
 fs.mkdir('dist', { recursive: true }).then(() => {
   // copy main source file
   fs.writeFile(path.join('dist', srcName), input)
 
   // write min file and source map
   const outPath = path.join('dist', outName)
-  if (fss.existsSync(outPath)) {
-    const beforeSize = fss.statSync(outPath).size
-    console.log(
-      [
-        '== zaftig.min.js ==',
-        'Before: ' + beforeSize,
-        ' After: ' + output.code.length,
-        '  Diff: ' + (beforeSize - output.code.length),
-        '==================='
-      ].join('\n')
-    )
-  }
+  reportDiff(outPath, output.code)
   fs.writeFile(outPath, output.code)
   fs.writeFile(outPath + '.map', output.map)
 })
